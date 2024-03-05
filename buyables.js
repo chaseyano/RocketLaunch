8a4f5de85e2ac47fa8cf35104528de5b9147b39a
SrivatsaChilla <srivatsa.chilla@gmail.com>
2024-03-05 15:58:51 -0800

    Made prices more realistic in buyables

--- buyables.js
+++ buyables.js
@@ -1,17 +1,17 @@
 export const budget = 5000;
 
 export const materialPrices = {
-    titanium: 100,
-    aluminium: 100,
-    steel: 100,
-    carbon: 100,
-    silica: 100
+    titanium: 1000,   // Price per unit (in dollars)
+    aluminium: 500,   // Price per unit (in dollars)
+    steel: 300,       // Price per unit (in dollars)
+    carbon: 700,      // Price per unit (in dollars)
+    silica: 200       // Price per unit (in dollars)
 };
 
 export const fuelPrices = {
-    hydrogen: 1200,
-    kerosene: 100,
-    methane: 100,
-    helium3: 100,
-    rp1: 100
+    hydrogen: 0.01,   // Price per unit (in dollars)
+    kerosene: 1.2,    // Price per unit (in dollars)
+    methane: 0.8,     // Price per unit (in dollars)
+    helium3: 2000,    // Price per unit (in dollars)
+    rp1: 1.5         // Price per unit (in dollars)
 };
